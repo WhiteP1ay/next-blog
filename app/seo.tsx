57fbd14ac6d21a6c21477ad533e4802aea37db4b
piaohz <piaohz@ijiwei.com>
2025-03-03 15:38:22 +0800

    feat: 定制化

--- app/seo.tsx
+++ app/seo.tsx
@@ -19,13 +19,8 @@ export function genPageMetadata({ title, description, image, ...rest }: PageSEOP
       url: './',
       siteName: siteMetadata.title,
       images: image ? [image] : [siteMetadata.socialBanner],
-      locale: 'en_US',
+      locale: 'zh_CN',
       type: 'website',
-    },
-    twitter: {
-      title: `${title} | ${siteMetadata.title}`,
-      card: 'summary_large_image',
-      images: image ? [image] : [siteMetadata.socialBanner],
     },
     ...rest,
   }
